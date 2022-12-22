8295bf8045afd603953e8323437d62d1ded552fc
Arman Taheri <armantahery1381@gmail.com>
2022-12-22 14:40:39 +0330

    date format is changed to YYYY-MM-DD and expiration date is added

--- script2.sh
+++ script2.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 read -p "Enter the number of users to create: " num
-echo -e "$(date)\n SSH Host:\t$(hostname -I)\n $(grep -w Port /etc/ssh/sshd_config)\n\n">> vpn.txt
+echo -e "$(date +%Y-%m-%d)\n SSH Host:\t$(hostname -I)\n $(grep -w Port /etc/ssh/sshd_config)\nexpiration\t$(date -d "38 days +"%Y-%m-%d)\n\n">> vpn.txt
 for i in $(seq 1 $num)
 do
     username=$(pwgen -1)
@@ -11,4 +11,4 @@ do
     echo -e "$username\thard\tmaxlogins\t1">> /etc/security/limits.conf
     
 done
-echo -e "$num acccouns has been created"
+echo -e "$num acccounts has been created"
