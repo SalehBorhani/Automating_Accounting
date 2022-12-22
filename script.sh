89a3bf315bd7edf37e6e7e3061b3dea997f86959
SalehBorhani <borhanisaleh6@gmail.com>
2022-12-22 20:07:02 +0330

    Updated the script.sh

--- script.sh
+++ script.sh
@@ -3,9 +3,9 @@ read -p "Enter the number of users to create: " num
 
 
 echo -e "$(date +%Y-%m-%d)\nSSH Host:\n$(hostname -I | cut -d ' ' -f 1)\nPort: $(grep -w Port /etc/ssh/sshd_config | cut -d ' ' -f 2)\nExpiration: $(date -d "38 days" +"%Y-%m-%d")\n\n">> vpn.log
-echo -e "$(date +%Y-%m-%d)\nSSH Host:\n$(hostname -I | cut -d ' ' -f 1)\nPort: $(grep -w Port /etc/ssh/sshd_config | cut -d ' ' -f 2)\nExpiration: $(date -d "38 days" +"%Y-%m-%d")\n\n" > vpn.txt
 for i in $(seq 1 $num)
 do
+    echo -e "$(date +%Y-%m-%d)\nSSH Host:\n$(hostname -I | cut -d ' ' -f 1)\nPort: $(grep -w Port /etc/ssh/sshd_config | cut -d ' ' -f 2)\nExpiration: $(date -d "38 days" +"%Y-%m-%d")\n\n" > vpn.txt
     username=$(pwgen -1)
     password=$username
     useradd -s /bin/false -e `date -d "38 days" +"%Y-%m-%d"` $username
@@ -14,8 +14,8 @@ do
     echo -e "Username: $username \nPassword: $password\n" >> vpn.txt
     echo -e "$username\thard\tmaxlogins\t1">> /etc/security/limits.conf
     echo -e "Username: $username \nPassword: $password\n" 
-    
+    python3 bot.py
 done
 echo -e "$num acccounts has been created"
-python3 bot.py
+
 
