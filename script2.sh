a205b216cce6a1fc84893925b40fbeed49bf2b3f
Arman Taheri <armantahery1381@gmail.com>
2022-12-22 19:02:33 +0330

    multiple ip problem has been solved

--- script2.sh
+++ script2.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 read -p "Enter the number of users to create: " num
-echo -e "$(date +%Y-%m-%d)\n SSH Host:\t$(hostname -I)\n $(grep -w Port /etc/ssh/sshd_config)\nexpiration\t$(date -d "38 days" +"%Y-%m-%d")\n\n">> vpn.txt
-echo -e "$(date +%Y-%m-%d)\n SSH Host:\t$(hostname -I)\n $(grep -w Port /etc/ssh/sshd_config)\nexpiration\t$(date -d "38 days" +"%Y-%m-%d")\n\n"
+echo -e "$(date +%Y-%m-%d)\n SSH Host:\t$(hostname -I| cut -d' ' -f1)\n $(grep -w Port /etc/ssh/sshd_config)\nexpiration\t$(date -d "38 days" +"%Y-%m-%d")\n\n">> vpn.txt
+echo -e "$(date +%Y-%m-%d)\n SSH Host:\t$(hostname -I| cut -d' ' -f1\n) $(grep -w Port /etc/ssh/sshd_config)\nexpiration\t$(date -d "38 days" +"%Y-%m-%d")\n\n"
 for i in $(seq 1 $num)
 do
     username=$(pwgen -1)
