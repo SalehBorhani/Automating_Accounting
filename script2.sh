b1ef672c9146daf03baad210beded2eb28b18295
Arman Taheri <armantahery1381@gmail.com>
2022-12-22 15:57:07 +0330

    users and passwords will be shown in terminal

--- script2.sh
+++ script2.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 read -p "Enter the number of users to create: " num
 echo -e "$(date +%Y-%m-%d)\n SSH Host:\t$(hostname -I)\n $(grep -w Port /etc/ssh/sshd_config)\nexpiration\t$(date -d "38 days" +"%Y-%m-%d")\n\n">> vpn.txt
+echo -e "$(date +%Y-%m-%d)\n SSH Host:\t$(hostname -I)\n $(grep -w Port /etc/ssh/sshd_config)\nexpiration\t$(date -d "38 days" +"%Y-%m-%d")\n\n"
 for i in $(seq 1 $num)
 do
     username=$(pwgen -1)
@@ -9,6 +10,7 @@ do
     echo "${username}:${password}" | chpasswd
     echo -e "Username: $username \tPassword: $password\n" >> vpn.txt
     echo -e "$username\thard\tmaxlogins\t1">> /etc/security/limits.conf
+    echo -e "Username: $username \tPassword: $password\n" 
     
 done
 echo -e "$num acccounts has been created"
